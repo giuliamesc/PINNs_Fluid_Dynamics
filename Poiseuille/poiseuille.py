7343cc6f4fe06a3491fa48746be5de2734ad7249
Luca Sosta <luca.sosta@mail.polimi.it>
2021-04-24 19:11:49 +0200

    Correzione losses

--- Poiseuille/poiseuille.py
+++ Poiseuille/poiseuille.py
@@ -9,46 +9,55 @@ from nisaba.experimental.physics import tens_style as operator
 import tensorflow as tf
 import numpy as np
 
-
 #############################################################################
-#  (u * u_x + v * u_y) - mu * (u_xx + u_yy) = -p_x      in \Omega = (0, L) x (0, 2*delta)
-#  rho * (u * v_x + v * v_y) - mu * (v_xx + v_yy) = -p_y      in \Omega = (0, L) x (0, 2*delta)
-#  u   = v   = 0                                              in (0, L) x {0, 2*delta}
-#  u_x = v_x = 0                                              in {0, L} x (0, 2*delta)
+#  u_x + v_y = 0                                           in \Omega = (0, 1) x (0, 2*delta)
+#  (u * u_x + v * u_y) - (u_xx + u_yy) / Re + p_x = 0      in \Omega = (0, 1) x (0, 2*delta)
+#  (u * v_x + v * v_y) - (v_xx + v_yy) / Re + p_y = 0      in \Omega = (0, 1) x (0, 2*delta)
+#  u   = v   = 0                                              in (0, 1) x {0, 2*delta}
+#  u_x = v_x = 0                                              in {0, 1} x (0, 2*delta)
+#  p = p_str                                                  in {0} x (0, 2*delta)
+#  p = p_end                                                  in {1} x (0, 2*delta)
+#  p_y = 0                                                    in (0, 1) x {0, 2*delta}
 #
-# u_exact(x,y) = - p_x * y * (2 - y / delta) * delta / (2*mu)
-# v_exact(x,y) = 0 
+#  p_exact(x,y) = (p_end-p_str)/L * x + p_str
+#  u_exact(x,y) = - Re * p_x * y * (2 - y / delta) * delta / 2
+#  v_exact(x,y) = 0 
 #############################################################################
 
 # %% Options
-# Domain Setup
-L      = 1
-delta  = 0.05
-H = 2 * delta
-# Experiment setup
-P_str = 1000000
+# Fluid and Flow Setup
+dim   = 2
+rho   = 3100  # lava density
+mu    = 890   # lava viscosity
+Ub    = 1     # Bulk velocity
+L_dim = 1     # length of the pipe
+H_dim = 0.1   # heigth of the pipe
+P_str = 1e6 
 P_end = 0
-# Fluid Setup (lava)
-rho = 3100
-mu  = 890
 
-# Numerical options
+# Adimensionalization
+Re = rho * Ub * L_dim / mu
+L = 1
+H = H_dim / L_dim
+delta = H / 2
+p_str = P_str / (rho * Ub^2)
+p_end = P_end / (rho * Ub^2)
+
+# %% Forcing and Solutions
+p_x = p_end - p_str
+
+forcing_x = lambda x: 0*x[:,0]
+forcing_y = lambda x: 0*x[:,0] 
+
+p_exact   = lambda x: (p_end-p_str)/L * x[:,0] + p_str
+u_exact   = lambda x: - Re * p_x * x[:,1] * (2 - x[:,1] / delta) * delta / 2
+v_exact   = lambda x: 0*x[:,0]
+ 
+# %% Numerical options
 num_PDE  = 200
 num_BC   = 20 # points for each edge
 num_hint = 50
 num_test = 1000
-
-
-# %% Forcing and Solutions
-dim = 2
-p_x = (P_end - P_str) / L
-p_y = 0
-forcing_x = lambda x: -p_x + 0*x[:,0]
-forcing_y = lambda x: -p_y + 0*x[:,0] 
-
-u_exact   = lambda x: -p_x * x[:,1] * (2 - x[:,1] / delta) * delta / (2*mu)
-v_exact   = lambda x: 0*x[:,0]
- 
 
 # %% Inizialization
 
@@ -60,7 +69,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(20, input_shape=(2,), activation=tf.nn.tanh),
     tf.keras.layers.Dense(20, activation=tf.nn.tanh),
     tf.keras.layers.Dense(20, activation=tf.nn.tanh),
-    tf.keras.layers.Dense(2)
+    tf.keras.layers.Dense(3)
 ])
 
 x_PDE   = tf.random.uniform(shape = [num_PDE,  2], minval = [0, 0],  maxval = [L, H], dtype = ns.config.get_dtype())
@@ -71,106 +80,130 @@ x_BC_y0 = tf.random.uniform(shape = [num_BC,   2], minval = [0, 0],  maxval = [L
 x_BC_y1 = tf.random.uniform(shape = [num_BC,   2], minval = [0, H],  maxval = [L, H], dtype = ns.config.get_dtype())
 x_test  = tf.random.uniform(shape = [num_test, 2], minval = [0, 0],  maxval = [L, H], dtype = ns.config.get_dtype())
 
-
-u_test = u_exact(x_test)[:, None]
-v_test = v_exact(x_test)[:, None]
-u_hint = u_exact(x_hint)[:, None]
-v_hint = v_exact(x_hint)[:, None]
-inlet = u_exact(x_BC_x0)[:, None]
-f_1 = forcing_x(x_PDE)
-f_2 = forcing_y(x_PDE)
-
 # %% Losses creation
 
-def PDE(x, k, force):  # k is the coordinate of the vectorial equation
+def create_rhs(x, force):
+    if force is None:
+        samples = x.shape[0]
+        return tf.zeros(shape = [samples,1], dtype = ns.config.get_dtype())
+    return force(x)
+
+# k is the coordinate of the vectorial equation
+# j is the direction in which the derivative is computed
+
+def PDE_MASS(x):
+    with ns.GradientTape(persistent=True) as tape:
+        tape.watch(x_PDE)
+        u_vect = model(x_PDE)[:,0:2]
+        div = operator.divergence_vector(tape, u_vect, x_PDE, dim)
+    return div
+
+def PDE_MOM(x, k, force):
     with ns.GradientTape(persistent=True) as tape:
         tape.watch(x)
+        
         u_vect = model(x)
         u = u_vect[:,0]
         v = u_vect[:,1]
+        p = u_vect[:,2]
         u_eq = u_vect[:,k]
+        
         grad_eq = operator.gradient_scalar(tape, u_eq, x)
+        dp   = operator.gradient_scalar(tape, p, x)[:,k]
         deqx = grad_eq[:,0]
         deqy = grad_eq[:,1]
         lapl_eq = operator.laplacian_scalar(tape, u_eq, x, dim)
-    return tf.math.abs(rho * (u * deqx + v * deqy) - mu * (lapl_eq) - force)
+        
+        rhs = create_rhs(x, force)
+    return (u * deqx + v * deqy) - (lapl_eq) / Re + dp - rhs
 
 def BC_D(x, k, g_bc = None):
+    uk = model(x)[:,k]
+    rhs = create_rhs(x, g_bc)
+    return uk - rhs
+
+def BC_N(x, k, j, g_bc = None):
     with ns.GradientTape(persistent = True) as tape:
-        if g_bc is None:
-            samples = x.shape[0]
-            g_bc = tf.zeros(shape = [samples,1], dtype = ns.config.get_dtype())
         tape.watch(x)
-        u = model(x)[:,k]
-        return tf.math.abs(u - g_bc)
-    
+        uk = model(x)[:,k]
+        uk_j = operator.gradient_scalar(tape, uk, x)[:,j]
+        rhs = create_rhs(x, g_bc)
+        return uk_j - rhs
 
-def BC_N():
-    with ns.GradientTape(persistent = True) as tape:
-        tape.watch(x_BC_x1)
-        u_vect = model(x_BC_x1)
-        grad_u_vect = operator.gradient_vector(tape, u_vect, x_BC_x1, dim)
-        u_x = grad_u_vect[:,0,0]
-        v_x = grad_u_vect[:,1,0]
-    return tf.math.abs(u_x) + tf.math.abs(v_x)
+def exact_value(x, k, sol = None):
+    uk = model(x)[:,k]
+    rhs = create_rhs(x, sol)
+    return uk - rhs
 
-def Hints():
-    with ns.GradientTape(persistent = True) as tape:
-        tape.watch(x_hint)
-        u_vect = model(x_hint)
-        u = u_vect[:,0]
-        v = u_vect[:,1] 
-    return (u - u_hint) * (u - u_hint) + (v - v_hint) * (v - v_hint)
+# %% Training Losses definition
+PDE_losses = [ns.LossMeanSquares('PDE_MASS', lambda: PDE_MASS(x_PDE), normalization = 1e4, weight = 1.0),
+              ns.LossMeanSquares('PDE_MOMU', lambda: PDE_MOM(x_PDE, 0, forcing_x), normalization = 1e4, weight = 1.0),
+              ns.LossMeanSquares('PDE_MOMV', lambda: PDE_MOM(x_PDE, 1, forcing_y), normalization = 1e4, weight = 1.0)]
+BCD_losses = [ns.LossMeanSquares('BCD_x0_u', lambda: BC_D(x_BC_x0,0, u_exact), weight = 1.0),
+              ns.LossMeanSquares('BCD_x0_v', lambda: BC_D(x_BC_x0,1), weight = 1.0),
+              ns.LossMeanSquares('BCD_y0_u', lambda: BC_D(x_BC_y0,0), weight = 1.0),
+              ns.LossMeanSquares('BCD_y0_v', lambda: BC_D(x_BC_y0,1), weight = 1.0),
+              ns.LossMeanSquares('BCD_y1_u', lambda: BC_D(x_BC_y1,0), weight = 1.0),
+              ns.LossMeanSquares('BCD_y1_v', lambda: BC_D(x_BC_y1,1), weight = 1.0)]
+BCN_losses = [ns.LossMeanSquares('BCN_x1_u', lambda: BC_N(x_BC_x1,0,0), weight = 1.0),
+              ns.LossMeanSquares('BCN_x1_v', lambda: BC_N(x_BC_x1,1,0), weight = 1.0)]
+EXC_Losses = [ns.LossMeanSquares( 'exact_u', lambda: exact_value(x_hint, 0, u_exact), weight = 1.0),
+              ns.LossMeanSquares( 'exact_v', lambda: exact_value(x_hint, 1, v_exact), weight = 1.0)]
 
+losses = PDE_losses + BCD_losses + BCN_losses + EXC_Losses
 
-def test_loss():
-    u_vect = model(x_test)
-    u = u_vect[:,0]
-    v = u_vect[:,1]
-    return (u - u_test) * (u - u_test) + (v - v_test) * (v - v_test)
-
-# %% Losses definition
-losses = [ns.LossMeanSquares(' PDE_U', lambda: PDE(x_PDE, 0, f_1), weight = 2.0, normalization = num_PDE),
-          ns.LossMeanSquares(' PDE_V', lambda: PDE(x_PDE, 1, f_2), weight = 2.0, normalization = num_PDE),
-          ns.LossMeanSquares('BCD_x0', lambda: BC_D(x_BC_x0, 0, inlet) + BC_D(x_BC_x0, 1) , weight = 10.0, normalization = num_BC),
-          ns.LossMeanSquares('BCD_y0', lambda: BC_D(x_BC_y0, 0) + BC_D(x_BC_y0, 1) , weight = 10.0, normalization = num_BC),
-          ns.LossMeanSquares('BCD_y1', lambda: BC_D(x_BC_y1, 0) + BC_D(x_BC_y1, 1) , weight = 10.0, normalization = num_BC),
-          ns.LossMeanSquares( 'BC_N',  BC_N, weight = 10.0, normalization = num_BC),
-          ns.LossMeanSquares('Hints', Hints, weight = 15.0, normalization = num_hint)
-          ]
-loss_test = ns.LossMeanSquares('fit', test_loss, normalization = num_test)
-
+# %% Test Losses definition
+loss_test = [ns.LossMeanSquares('u_fit', lambda: exact_value(x_test, 0, u_exact)),
+             ns.LossMeanSquares('v_fit', lambda: exact_value(x_test, 1, v_exact)),
+             ns.LossMeanSquares('p_fit', lambda: exact_value(x_test, 2, p_exact))]
 # %% Training
 pb = ns.OptimizationProblem(model.variables, losses, loss_test)
 
 ns.minimize(pb, 'keras', tf.keras.optimizers.Adam(learning_rate=1e-2), num_epochs = 100)
 ns.minimize(pb, 'scipy', 'L-BFGS-B', num_epochs = 500)
 
-
 # %% Saving Loss History
 
-problem_name = "Poiseuille"
+problem_name = "Poiseuille_Adimensional_Pressure"
 history_file = os.path.join(cwd, "{}_history_loss.json".format(problem_name))
 pb.save_history(history_file)
 ns.utils.plot_history(history_file)
 history = ns.utils.load_json(history_file)
 
 # %% Post-processing
-import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = fig.add_subplot(121, projection='3d')
-ax.scatter(x_test[:,0], x_test[:,1], u_test, label = 'exact solution')
-ax.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,0].numpy(), label = 'numerical solution')
-ax.legend()
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('velocity u')
-ax = fig.add_subplot(122, projection='3d')
-ax.scatter(x_test[:,0], x_test[:,1], v_test, label = 'exact solution')
-ax.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,1].numpy(), label = 'numerical solution')
-ax.legend()
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('velocity v')
 
-plt.show(block = True)
+p_test   = p_exact(x_test)[:, None]
+u_test   = u_exact(x_test)[:, None]
+v_test   = v_exact(x_test)[:, None]
+
+import matplotlib.pyplot as plt
+
+fig_1 = plt.figure(1)
+ax_1 = fig_1.add_subplot(projection='3d')
+ax_1.scatter(x_test[:,0], x_test[:,1], u_test, label = 'exact solution')
+ax_1.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,0].numpy(), label = 'numerical solution')
+ax_1.legend()
+ax_1.set_xlabel('x')
+ax_1.set_ylabel('y')
+ax_1.set_zlabel('velocity u')
+
+fig_2 = plt.figure(2)
+ax_2 = fig_2.add_subplot(projection='3d')
+ax_2.scatter(x_test[:,0], x_test[:,1], v_test, label = 'exact solution')
+ax_2.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,1].numpy(), label = 'numerical solution')
+ax_2.legend()
+ax_2.set_xlabel('x')
+ax_2.set_ylabel('y')
+ax_2.set_zlabel('velocity v')
+
+fig_3 = plt.figure(3)
+ax_3 = fig_3.add_subplot(projection='3d')
+ax_3.scatter(x_test[:,0], x_test[:,1], p_test, label = 'exact solution')
+ax_3.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,2].numpy(), label = 'numerical solution')
+ax_3.legend()
+ax_3.set_xlabel('x')
+ax_3.set_ylabel('y')
+ax_3.set_zlabel('pressure')
+
+plt.show(block = False)
+print("Reynolds Number -> {}".format(Re))
--- Poiseuille/poiseuille_no_pressure.py
+++ Poiseuille/poiseuille_no_pressure.py
@@ -75,74 +75,71 @@ x_BC_y0 = tf.random.uniform(shape = [num_BC,   2], minval = [0, 0],  maxval = [L
 x_BC_y1 = tf.random.uniform(shape = [num_BC,   2], minval = [0, H],  maxval = [L, H], dtype = ns.config.get_dtype())
 x_test  = tf.random.uniform(shape = [num_test, 2], minval = [0, 0],  maxval = [L, H], dtype = ns.config.get_dtype())
 
-
-u_test = u_exact(x_test)[:, None]
-v_test = v_exact(x_test)[:, None]
-u_hint = u_exact(x_hint)[:, None]
-v_hint = v_exact(x_hint)[:, None]
-inlet = u_exact(x_BC_x0)[:, None]
-f_1 = forcing_x(x_PDE)
-f_2 = forcing_y(x_PDE)
-
 # %% Losses creation
 
-def PDE(x, k, force):  # k is the coordinate of the vectorial equation
+def create_rhs(x, force):
+    if force is None:
+        samples = x.shape[0]
+        return tf.zeros(shape = [samples,1], dtype = ns.config.get_dtype())
+    return force(x)
+
+# k is the coordinate of the vectorial equation
+# j is the direction in which the derivative is computed
+
+def PDE_MOM(x, k, force):
     with ns.GradientTape(persistent=True) as tape:
         tape.watch(x)
+        
         u_vect = model(x)
         u = u_vect[:,0]
         v = u_vect[:,1]
         u_eq = u_vect[:,k]
+        
         grad_eq = operator.gradient_scalar(tape, u_eq, x)
         deqx = grad_eq[:,0]
         deqy = grad_eq[:,1]
         lapl_eq = operator.laplacian_scalar(tape, u_eq, x, dim)
-    return (u * deqx + v * deqy) - (lapl_eq) / Re - force
+        
+        rhs = create_rhs(x, force)
+    return (u * deqx + v * deqy) - (lapl_eq) / Re - rhs
 
 def BC_D(x, k, g_bc = None):
-    with ns.GradientTape(persistent = True) as tape:
-        if g_bc is None:
-            samples = x.shape[0]
-            g_bc = tf.zeros(shape = [samples,1], dtype = ns.config.get_dtype())
-        tape.watch(x)
-        uk = model(x)[:,k]
-        return tf.math.abs(uk - g_bc)
+    uk = model(x)[:,k]
+    rhs = create_rhs(x, g_bc)
+    return uk - rhs
 
 def BC_N(x, k, j, g_bc = None):
     with ns.GradientTape(persistent = True) as tape:
-        if g_bc is None:
-            samples = x.shape[0]
-            g_bc = tf.zeros(shape = [samples,1], dtype = ns.config.get_dtype())
         tape.watch(x)
         uk = model(x)[:,k]
         uk_j = operator.gradient_scalar(tape, uk, x)[:,j]
-        return tf.math.abs(uk_j - g_bc)
+        rhs = create_rhs(x, g_bc)
+        return uk_j - rhs
 
-def Hints():
-    with ns.GradientTape(persistent = True) as tape:
-        tape.watch(x_hint)
-        u_vect = model(x_hint)
-        u = u_vect[:,0]
-        v = u_vect[:,1] 
-    return (u - u_hint) * (u - u_hint) + (v - v_hint) * (v - v_hint)
+def exact_value(x, k, sol = None):
+    uk = model(x)[:,k]
+    rhs = create_rhs(x, sol)
+    return uk - rhs
 
-def test_loss():
-    u_vect = model(x_test)
-    u = u_vect[:,0]
-    v = u_vect[:,1]
-    return (u - u_test) * (u - u_test) + (v - v_test) * (v - v_test)
+# %% Training Losses definition
+PDE_losses = [ns.LossMeanSquares('PDE_MOMU', lambda: PDE_MOM(x_PDE, 0, forcing_x), normalization = 1e4, weight = 1.0),
+              ns.LossMeanSquares('PDE_MOMV', lambda: PDE_MOM(x_PDE, 1, forcing_y), normalization = 1e4, weight = 1.0)]
+BCD_losses = [ns.LossMeanSquares('BCD_x0_u', lambda: BC_D(x_BC_x0,0, u_exact), weight = 1.0),
+              ns.LossMeanSquares('BCD_x0_v', lambda: BC_D(x_BC_x0,1), weight = 1.0),
+              ns.LossMeanSquares('BCD_y0_u', lambda: BC_D(x_BC_y0,0), weight = 1.0),
+              ns.LossMeanSquares('BCD_y0_v', lambda: BC_D(x_BC_y0,1), weight = 1.0),
+              ns.LossMeanSquares('BCD_y1_u', lambda: BC_D(x_BC_y1,0), weight = 1.0),
+              ns.LossMeanSquares('BCD_y1_v', lambda: BC_D(x_BC_y1,1), weight = 1.0)]
+BCN_losses = [ns.LossMeanSquares('BCN_x1_u', lambda: BC_N(x_BC_x1,0,0), weight = 1.0),
+              ns.LossMeanSquares('BCN_x1_v', lambda: BC_N(x_BC_x1,1,0), weight = 1.0)]
+EXC_Losses = [ns.LossMeanSquares( 'exact_u', lambda: exact_value(x_hint, 0, u_exact), weight = 1.0),
+              ns.LossMeanSquares( 'exact_v', lambda: exact_value(x_hint, 1, v_exact), weight = 1.0)]
 
-# %% Losses definition
-losses = [ns.LossMeanSquares(' PDE_U', lambda: PDE(x_PDE, 0, f_1), weight = 1.0),
-          ns.LossMeanSquares(' PDE_V', lambda: PDE(x_PDE, 1, f_2), weight = 1.0),
-          ns.LossMeanSquares('BCN_x0_x', lambda: BC_N(x_BC_x0,0,0), weight = 5.0),
-          ns.LossMeanSquares('BCD_x0_y', lambda: BC_D(x_BC_x0,1), weight = 5.0),
-          ns.LossMeanSquares('BCD_y0', lambda: BC_D(x_BC_y0,0  ) + BC_D(x_BC_y0,1  ), weight = 10.0),
-          ns.LossMeanSquares('BCD_y1', lambda: BC_D(x_BC_y1,0  ) + BC_D(x_BC_y1,1  ), weight = 10.0),
-          ns.LossMeanSquares( 'BC_N',  lambda: BC_N(x_BC_x1,0,0) + BC_N(x_BC_x1,1,0), weight = 10.0),
-          #ns.LossMeanSquares('Hints', lambda: Hints(), weight = 15.0)
-          ]
-loss_test = ns.LossMeanSquares('fit', test_loss, normalization = num_test)
+losses = PDE_losses + BCD_losses + BCN_losses + EXC_Losses
+
+# %% Test Losses definition
+loss_test = [ns.LossMeanSquares('u_fit', lambda: exact_value(x_test, 0, u_exact)),
+             ns.LossMeanSquares('v_fit', lambda: exact_value(x_test, 1, v_exact))]
 
 # %% Training
 pb = ns.OptimizationProblem(model.variables, losses, loss_test)
@@ -159,22 +156,27 @@ ns.utils.plot_history(history_file)
 history = ns.utils.load_json(history_file)
 
 # %% Post-processing
+u_test = u_exact(x_test)
+v_test = v_exact(x_test)
+
 import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = fig.add_subplot(121, projection='3d')
-ax.scatter(x_test[:,0], x_test[:,1], u_test, label = 'exact solution')
-ax.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,0].numpy(), label = 'numerical solution')
-ax.legend()
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('velocity u')
-ax = fig.add_subplot(122, projection='3d')
-ax.scatter(x_test[:,0], x_test[:,1], v_test, label = 'exact solution')
-ax.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,1].numpy(), label = 'numerical solution')
-ax.legend()
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('velocity v')
+fig_1 = plt.figure(1)
+ax_1 = fig_1.add_subplot(projection='3d')
+ax_1.scatter(x_test[:,0], x_test[:,1], u_test, label = 'exact solution')
+ax_1.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,0].numpy(), label = 'numerical solution')
+ax_1.legend()
+ax_1.set_xlabel('x')
+ax_1.set_ylabel('y')
+ax_1.set_zlabel('velocity u')
+
+fig_2 = plt.figure(2)
+ax_2 = fig_2.add_subplot(projection='3d')
+ax_2.scatter(x_test[:,0], x_test[:,1], v_test, label = 'exact solution')
+ax_2.scatter(x_test[:,0], x_test[:,1], model(x_test)[:,1].numpy(), label = 'numerical solution')
+ax_2.legend()
+ax_2.set_xlabel('x')
+ax_2.set_ylabel('y')
+ax_2.set_zlabel('velocity v')
 
 plt.show(block = True)
 print("Reynolds Number -> {}".format(Re))
