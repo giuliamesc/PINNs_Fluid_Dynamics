7d9651a6da5623432d9d0d9c7b1e4d8346aabdb6
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2021-12-27 21:29:37 +0100

    style fix

--- DataGeneration/coronary_solver.py
+++ DataGeneration/coronary_solver.py
@@ -1,38 +1,23 @@
 #!/usr/bin/env python3
 import dolfin as df
-from dolfin import *
 import numpy as np
 
 #%% Options
 
-H_inlet = 0.4123
-theta_deg = 14.036
-theta = np.pi*theta_deg / 180
-sintheta = np.sin(theta)
-costheta = np.cos(theta)
-rad = np.sqrt(17)
-
 nu = 1e0
 T  = 1e-2
 U  = 1
-
 dt = 1e-4
 
 formulation = 'navier-stokes_SI'
-
 testcase = 'coronary'
 
 #%% Solver
 
-Mesh.MshFileVersion = 2.0;
-# import os
-# os.system('dolfin-convert coroParam.msh coroParam.xml')
-
 import meshio
 msh = meshio.read("coroParam.msh")
 meshio.write("coroParam.xml",msh)
-mesh = Mesh("coroParam.xml")
-print('Giulia')
+mesh = df.Mesh("coroParam.xml")
 
 V = df.VectorElement("CG", mesh.ufl_cell(), 2)
 Q = df.FiniteElement("CG", mesh.ufl_cell(), 1)
@@ -57,12 +42,16 @@ def outflow_boundary(x,on_boundary):
 def noslip_boundary(x,on_boundary):
     return on_boundary and not(inflow_boundary(x, on_boundary)) and not(outflow_boundary(x, on_boundary))
 
+# u(y) = y * (H-y) / H^2
 
-inflow_function = df.Expression(("costheta*1/rad*(x[0]+4*x[1])*(H-1/rad*(x[0]+4*x[1]))/std::pow(H,2)", "sintheta*1/rad*(x[0]+4*x[1])*(H-1/rad*(x[0]+4*x[1]))/std::pow(H,2)","0"), costheta = costheta, sintheta = sintheta, rad = rad, H = H_inlet, degree=2)
+inflow_function = df.Expression(("cos_theta*(1/rad*(x[0]+4*x[1]))*(H-1/rad*(x[0]+4*x[1]))/(H*H)", 
+                                 "sin_theta*(1/rad*(x[0]+4*x[1]))*(H-1/rad*(x[0]+4*x[1]))/(H*H)",
+                                 "0"), cos_theta = np.cos(np.arctan(1/4)), sin_theta = np.sin(np.arctan(1/4)), 
+                                       rad = np.sqrt(17), H = np.sqrt(0.4**2+0.1**2), degree=2)
 
 bcs.append(df.DirichletBC(W.sub(0), df.Constant((0, 0, 0)), noslip_boundary))
-bcs.append(df.DirichletBC(W.sub(0), inflow_function    , inflow_boundary))
-bcs.append(df.DirichletBC(W.sub(1), df.Constant(0)     , outflow_boundary))
+bcs.append(df.DirichletBC(W.sub(0), inflow_function       , inflow_boundary))
+bcs.append(df.DirichletBC(W.sub(1), df.Constant(0)        , outflow_boundary))
 
 (v, q) = df.TestFunctions(W)
 (u, p) = df.TrialFunctions(W)
@@ -71,29 +60,7 @@ w = df.Function(W)
 w_old = df.Function(W)
 
 
-#%% Export to csv
-
-# import pandas as pd
-
-# data_list = []
-# num_out_points = 100 #square root of the desired number of points
-# x_points = np.linspace(0, L , num_out_points)
-# y_points = np.linspace(0, H , num_out_points)
-
-# def create_csv_for_df(w, t):
-#     (u, p) = w.split()
-#     u_points = np.array([u(x,y) for y in y_points for x in x_points])
-#     p_points = np.array([p(x,y) for y in y_points for x in x_points])
-#     x_tab = np.array([x for y in y_points for x in x_points])
-#     y_tab = np.array([y for y in y_points for x in x_points])
-#     t_tab = np.array([t for y in y_points for x in x_points])
-#     data = pd.DataFrame({'t': t_tab,
-#                          'x': x_tab,
-#                          'y': y_tab,
-#                          'ux': u_points[:,0],
-#                          'uy': u_points[:,1],
-#                          'p' : p_points})
-#     return data
+#%% Save output
 
 def save_output(w, t, it):
     (u, p) = w.split()
@@ -109,8 +76,7 @@ def save_output(w, t, it):
 
 times = np.arange(0.0, T, step = dt)
 save_output(w, 0, 0)
-# temp_dataframe = create_csv_for_df(w, 0)
-# data_list.append(temp_dataframe)
+
 
 for i in range(1, len(times)):
     t = times[i]
@@ -148,9 +114,5 @@ for i in range(1, len(times)):
         df.solve(a == rhs, w, bcs)
 
     save_output(w, t, i)
-    # temp_dataframe = create_csv_for_df(w, t)
-    # data_list.append(temp_dataframe)
 
-# dataframe_output_file = 'data/UnsteadyCase/' + formulation + '_' + testcase + '_unsteady_r'
-# output_dataframe = pd.concat(data_list)
-# output_dataframe.to_csv(dataframe_output_file + '.csv', index = False)
+print('Done')
